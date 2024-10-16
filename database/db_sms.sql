f5894a66aa3ff185ff1982207552069fec7406f9
Your Name <you@example.com>
2024-10-16 18:43:43 +0800

    updated sql

--- database/db_sms.sql
+++ database/db_sms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 13, 2024 at 12:53 PM
+-- Generation Time: Oct 16, 2024 at 10:42 AM
 -- Server version: 8.2.0
 -- PHP Version: 8.2.13
 
@@ -22,6 +22,42 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `db_sms` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE `db_sms`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `allowances`
+--
+
+DROP TABLE IF EXISTS `allowances`;
+CREATE TABLE IF NOT EXISTS `allowances` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `athlete_id` int NOT NULL,
+  `message` text NOT NULL,
+  `status` enum('available','received') NOT NULL,
+  `created_user` int NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Truncate table before insert `allowances`
+--
+
+TRUNCATE TABLE `allowances`;
+--
+-- Dumping data for table `allowances`
+--
+
+INSERT INTO `allowances` (`id`, `athlete_id`, `message`, `status`, `created_user`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 8, 'Your allowance is now available for collection at the accounting department. Please ensure that you claim it within the next 5 days to avoid any inconvenience. Thank you', 'available', 1, '2024-10-15 11:42:12', '2024-10-15 11:42:12', NULL),
+(2, 7, 'Your allowance is now available for collection at the accounting department. Please ensure that you claim it within the next 5 days to avoid any inconvenience. Thank you', 'available', 1, '2024-10-15 11:42:12', '2024-10-15 11:42:12', NULL),
+(3, 6, 'Your allowance is now available for collection at the accounting department. Please ensure that you claim it within the next 5 days to avoid any inconvenience. Thank you', 'available', 1, '2024-10-15 11:42:12', '2024-10-15 11:42:12', NULL),
+(4, 5, 'Your allowance is now available for collection at the accounting department. Please ensure that you claim it within the next 5 days to avoid any inconvenience. Thank you', 'available', 1, '2024-10-15 11:42:12', '2024-10-15 11:42:12', NULL),
+(5, 4, 'Your allowance is now available for collection at the accounting department. Please ensure that you claim it within the next 5 days to avoid any inconvenience. Thank you', 'available', 1, '2024-10-15 11:42:12', '2024-10-15 11:42:12', NULL),
+(6, 3, 'Your allowance is now available for collection at the accounting department. Please ensure that you claim it within the next 5 days to avoid any inconvenience. Thank you', 'received', 1, '2024-10-15 11:42:12', '2024-10-15 13:14:35', NULL);
 
 -- --------------------------------------------------------
 
@@ -217,14 +253,14 @@ TRUNCATE TABLE `users`;
 
 INSERT INTO `users` (`id`, `active`, `role`, `email`, `password`, `status`, `created_at`, `updated_at`) VALUES
 (1, 1, 'admin', 'admin@admin.com', '$2y$10$IYU34hPsTdMq3m8aiYzyPODAf27lzTqQ6hWHxzvXmKtLgAjICaR8K', 'active', '2024-09-20 12:04:19', '2024-09-20 20:04:37'),
-(2, 1, 'coordinator', 'coor@coor.com', '$2y$10$IYU34hPsTdMq3m8aiYzyPODAf27lzTqQ6hWHxzvXmKtLgAjICaR8K', 'active', '2024-09-20 12:05:19', '2024-09-20 20:05:37'),
+(2, 1, 'coordinator', 'coor@coor.com', '$2y$10$vCsqgCr9HeiT020byWwuUe00LReYNyd1KRwvgLgy1X6t0h62Fx97K', 'active', '2024-09-20 12:05:19', '2024-10-14 20:45:39'),
 (3, 1, 'athlete', 'athlete@athlete.com', '$2y$10$d1uYX36StQLJ3XXX2QZUmuV0BpYFYmNA6M5JfBgGfsNXT6EidtKl.', 'active', '2024-09-20 12:17:04', '2024-10-06 20:03:42'),
 (4, 1, 'athlete', 'menaqypybe@mailinator.com', '$2y$10$vKjY46S2x7SUPGewgOeG5uasXnGTk2.AJ5QMYG.7c8NjsfaXV4V0m', 'active', '2024-09-20 12:17:26', '2024-09-24 19:28:59'),
 (5, 1, 'athlete', 'dehilycic@mailinator.com', '$2y$10$mi/KbBjka09bbiYUntLYI.I6ZJ8ldXFJ5Cw29g3DYpy.m4CLdYJwu', 'active', '2024-09-20 12:17:42', '2024-09-24 19:29:06'),
 (6, 1, 'athlete', 'cupymifiw@mailinator.com', '$2y$10$A6Pok/UABm7asEYmWjAnfu4FOixcRw6pQ8ydqm0t443lW8lOH7WpG', 'active', '2024-09-20 12:17:58', '2024-09-24 19:29:13'),
 (7, 1, 'athlete', 'roqevapoqu@mailinator.com', '$2y$10$ZVevGrF7Eo.k/VLbrq0E0OR6j.KuJMY4fUWVIBqVexpE.uoghR.5G', 'active', '2024-09-20 12:18:17', '2024-09-21 12:21:34'),
 (8, 1, 'athlete', 'jaduvoxydo@mailinator.com', '$2y$10$stGy56bO6MOXbL2sFi648ugM/DlNfLxLx.RPiflr1sxXctmSQJmpm', 'active', '2024-09-20 12:18:40', '2024-09-24 20:57:03'),
-(9, 1, 'coordinator', 'jecawes@mailinator.com', '$2y$10$esPk47x2EdJ1GJyUwOq5duLMUt4QJu9IyF6pkQh2rIIkeW2yuI1R.', 'inactive', '2024-09-21 08:48:43', '2024-09-21 16:49:09'),
+(9, 1, 'coordinator', 'qipowy@mailinator.com', '$2y$10$XyJFB520/rgvFztjKCsKGeoSdX61VDcj0OOfTQ6Nnk1LRb2pyH5Nm', 'active', '2024-09-21 08:48:43', '2024-10-14 21:05:37'),
 (12, 1, 'athlete', 'bujinuxop@mailinator.com', '$2y$10$G/sVGqFdTx/ors8.lRKDJOvmAmer6Tsj1mPS/AddTlLEsFMnAEX7.', 'pending', '2024-10-10 12:36:28', '2024-10-10 20:36:28');
 
 -- --------------------------------------------------------
@@ -270,14 +306,14 @@ TRUNCATE TABLE `user_info`;
 
 INSERT INTO `user_info` (`id`, `user_id`, `first_name`, `last_name`, `middle_name`, `gender`, `year_level`, `course`, `address`, `school`, `guardian`, `age`, `birthday`, `sport`, `phone_number`, `cor`, `psa`, `medical_cert`, `picture`, `created_at`, `updated_at`) VALUES
 (1, 1, 'Roanna', 'Barrett', '', 'male', '', '', 'Omnis accusamus rati', '', '', 30, '2000-06-22', '', '09116935294', NULL, NULL, NULL, NULL, '2024-09-20 12:04:19', '2024-10-10 22:35:46'),
-(2, 2, 'Fin', 'Smill', '', 'male', '', '', 'Omnis accusamus rati', '', '', 30, '2000-06-22', '', '09116935294', NULL, NULL, NULL, NULL, '2024-09-20 12:05:19', '2024-10-10 22:35:46'),
+(2, 2, 'Fin', 'Smill', '', 'female', '', '', 'Omnis accusamus rati', '', '', 30, '2000-06-22', '', '09116935294', NULL, NULL, NULL, '4251d39851e791dfcce566816c4488b9.jpg', '2024-09-20 12:05:19', '2024-10-14 20:56:52'),
 (3, 3, 'Madaline', 'Barry', 'Abra Eaton', 'male', '2nd', 'Non et nihil et nisi', 'Nemo ipsum ad at ea ', 'Dolor corrupti dolo', 'Dignissimos itaque n', 18, '2000-06-22', 'basket_ball', '09547782036', 'b0108a782ac1628530c2947eaf0da2c1.png', 'f4cab839a8e5bb4ab45928592b6407e8.png', '5a2515728cedf1e41b1da2f6d0beaea6.png', '19da2ac05f73872ed0e004537faf981b.png', '2024-09-20 12:17:04', '2024-10-10 22:35:29'),
 (4, 4, 'Bradley', 'Barrera', 'Cynthia Franks', 'male', '1st', 'Quisquam nulla aliqu', 'Pariatur Distinctio', 'In laudantium deser', 'Facilis qui est vol', 42, '2000-06-22', 'basket_ball', '09804430914', '23077630c91d38a5e59436f71c110e9d.png', '2e1f4f143a613b6415940deacdd1cbe4.png', '471b7b2050532af5d19579bcbf21ded5.png', '377563c42206e0642b657595b7dfbe08.png', '2024-09-20 12:17:26', '2024-10-10 22:35:46'),
 (5, 5, 'Sopoline', 'Mcgowan', 'Stone Daugherty', 'male', '2nd', 'Molestiae repellendu', 'Laboris ullam odit q', 'Velit elit sit saep', 'Do et neque architec', 31, '2000-06-22', 'base_ball', '09613485704', '25a409fbcb7df98ccdb667fe12a5cbcd.png', '60891d3a4383e96612e97f96c2e9c5f4.png', '709d26dc42a7defeed7cdcffc47aeea2.png', '693f5e0a83a5543880a66b0e6c7568ae.png', '2024-09-20 12:17:42', '2024-10-10 22:35:46'),
 (6, 6, 'Lana', 'Harrell', 'Maisie Tanner', 'female', '1st', 'Voluptas quas vero d', 'Sunt enim aperiam i', 'Eligendi qui consequ', 'Aspernatur quia vita', 30, '2000-06-22', 'base_ball', '09133025784', 'd1ff371044f6d9dc6d4b70e110dc5db8.png', 'b938e58e3034d2e3fcb7b6ee7a72f432.png', 'a836b6d275be28fb2706b050a12f6ce0.png', 'cca77c8971a703ae9af42c1069081dff.png', '2024-09-20 12:17:58', '2024-10-10 22:35:46'),
 (7, 7, 'Hayley', 'Mccarthy', 'Maryam Coleman', 'male', '5th', 'Qui quidem velit sin', 'Cupidatat porro nequ', 'Dolor tenetur eos c', 'Modi dolor corrupti', 26, '2000-06-22', 'basket_ball', '09363299945', 'dc5d85cfb703e0451da0ac03dc2a02f2.png', 'b7c89fe241bae06b465e9b784c7b7f0c.png', 'a6c2ab88ff46701dcbb0819930799eb5.png', 'fb84e5739f573de66154f18f3bc2c459.png', '2024-09-20 12:18:17', '2024-10-10 22:35:46'),
 (8, 8, 'Sonya', 'Oconnor', 'MacKenzie Mendez', 'female', '1st', 'Sed odio sapiente fu', 'Delectus qui et est', 'Sed nihil soluta qua', 'Architecto nemo quo ', 17, '2000-06-22', 'basket_ball', '09171164462', '3a0fabaa3e95bc15b3946c8069a2c20b.png', '41907b505a85676f820eac7909f52f8f.png', '1e275a23be7e21fdb8f6a8d598c532f7.png', '94556f33f94ba5573d16c95ca703e45d.png', '2024-09-20 12:18:40', '2024-10-10 22:35:46'),
-(9, 9, 'Rudyard', 'Burch', 'Montana Pena', 'female', '', '', 'Nesciunt ut dolorem', '', '', 35, '2000-06-22', '', '09291659067', NULL, NULL, NULL, 'a0f695a7d44891ca7e34a2facf0a6ef8.png', '2024-09-21 08:48:43', '2024-10-10 22:35:46'),
+(9, 9, 'Maryam', 'Chan', 'Lane Bridges', 'male', '', '', 'Quo qui sed molestia', '', '', 18, '2000-06-22', '', '09167025106', NULL, NULL, NULL, 'a0f695a7d44891ca7e34a2facf0a6ef8.png', '2024-09-21 08:48:43', '2024-10-14 21:05:37'),
 (10, 12, 'Hamish', 'Wagner', 'Hayfa Tillman', 'male', '2nd', 'Quo aut eveniet est', 'Suscipit laboris fac', 'In autem nulla reici', 'Minima libero expedi', 25, '2000-06-22', 'swimming', '09699733268', '5dc073477b86d8e99a6e922ee7bfec4f.pdf', 'e7af1a4f81ddc3d162b43269600d8960.pdf', '461ab4a716e85726420b65e0a7d16fd7.pdf', '674d61ff2660b1711c00bbb35f5de10e.jpg', '2024-10-10 12:36:28', '2024-10-10 22:35:46');
 COMMIT;
 
